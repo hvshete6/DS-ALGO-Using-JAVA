e38268f1ec75db02a66f8623b9ca8e4e1b16c6cf
Harshvardhan Shete <hvshete6@gmail.com>
2021-07-24 22:29:05 +0530

    Merge Two Sorted Arrays Without using extra space ADDED

---
+++ Array/Find-Duplicate-Number/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+
+class Solution {
+
+    void findDuplicate(int[] a) {
+        Arrays.sort(a);
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == a[i + 1]) {
+                System.out.print(a[i]);
+                break;
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] a = { 1, 1 };
+        Solution sol = new Solution();
+        sol.findDuplicate(a);
+
+    }
+}
