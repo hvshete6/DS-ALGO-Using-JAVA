8561172eb215147e6264576d3e5a05a3e2efaf56
hvshete6 <72643379+hvshete6@users.noreply.github.com>
2021-07-26 23:31:49 +0530

    Update Solution.java

--- Array/Find all pairs whose sum is equal to given number/Solution.java
+++ Array/Find all pairs whose sum is equal to given number/Solution.java
@@ -1,5 +1,26 @@
 import java.util.*;
 
+class Solution {
+    int pairsCount(int[] arr, int n, int k) {
+        int res = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            map.compute(i, (ke, v) -> {
+                if (v == null)
+                    return 1;
+                return v + 1;
+
+            });
+        }
+        for (int ele : arr) {
+            if (2 * ele == k) {
+                res += Math.max(map.getOrDefault(ele, 0) - 1, 0);
+            } else {
+                res += map.getOrDefault(k - ele, 0);
+            }
+        }
+        return res / 2;
+    }
 
 
     public static void main(String[] args) {
